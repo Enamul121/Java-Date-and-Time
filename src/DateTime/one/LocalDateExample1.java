deb9a1ac56fdb081f108cef5e6babdecead273fc
Enamul121 <enamul172@gmail.com>
2019-02-17 17:47:37 +0600

    Initial commit

---
+++ src/DateTime/one/LocalDateExample1.java
@@ -0,0 +1,17 @@
+package DateTime.one;
+
+import java.time.LocalDate;
+
+public class LocalDateExample1 {
+
+    public static void main(String[] args) {
+
+        LocalDate date = LocalDate.now();
+
+
+
+        System.out.println("Today date: "+date);
+
+    }
+
+}
